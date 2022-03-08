698cd40bd0ecef3267b1ea80999cb25466a48837
Clayton Thebert <thebert.3@buckeyemail.osu.edu>
2022-03-07 19:59:47 -0500

    Fulfillment

---
+++ frontend/src/Fulfillment/data.tsx
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    fulfillment: [{
+        f_id: 1,
+        user: {
+            name: 'Michael'
+        },
+        isDelivered: true,
+        deliveredDate: "02/22/2022",
+    },
+    {
+        f_id: 2,
+        user: {
+            name: 'Kyle'
+        },
+        isDelivered: false,
+        deliveredDate: null,
+    },
+    {
+        f_id: 3,
+        user: {
+            name: 'Tim'
+        },
+        isDelivered: true,
+        deliveredDate: "02/21/2022",
+    }]
+}
